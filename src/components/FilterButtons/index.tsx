4653b87b0283e7a5a0b3bb3ff021ba41bd365466
Igor <iigormazetti@hotmail.com>
2022-07-06 11:41:19 -0300

    feat: implementada a lógica do botão all para trazer todos os pokemons

--- src/components/FilterButtons/index.tsx
+++ src/components/FilterButtons/index.tsx
@@ -1,13 +1,20 @@
+import { usePokemons } from "../../context/PokemonsContext";
+import { getPokemons } from "../../services/index";
 import { Container } from "./styles";
 
 import React from "react";
 
-// import { Container } from './styles';
-
 const FilterButtons: React.FC = () => {
+  const { setPokemons } = usePokemons();
+  const getAll = async () => {
+    const pokes = await getPokemons(0);
+    setPokemons(pokes);
+  };
   return (
     <Container>
-      <button type="button">All</button>
+      <button type="button" onClick={getAll}>
+        All
+      </button>
     </Container>
   );
 };
